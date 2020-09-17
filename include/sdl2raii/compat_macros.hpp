d674cb804c08be7bcdeb3dc910855b6f1976cd75
Riley Levy <rileylevy95@gmail.com>
2020-09-17 01:29:06 -0400

    Add fallback defns for LIKELY, UNLIKELY

--- include/sdl2raii/compat_macros.hpp
+++ include/sdl2raii/compat_macros.hpp
@@ -1,4 +1,9 @@
 #pragma once
 
+#if defined(__GNUC__) || defined(__clang__)
 #define SDLRAII_LIKELY(x) __builtin_expect(!!(x),1)
 #define SDLRAII_UNLIKELY(x) __builtin_expect(!!(x),0)
+#else
+#define SDLRAII_LIKELY(X) X
+#define SDLRAII_UNLIKELY(X) X
+#endif
